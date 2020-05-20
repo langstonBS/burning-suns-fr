3ef635fc9f04be7b947f313a3e4d74119f0cdc9e
langstonBS <langston.beckwithstanley@gmail.com>
2020-05-20 15:39:24 -0700

    othenticaton routs and navagation bar

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
 import {
     Link
-  } from "react-router-dom"
+} from "react-router-dom"
+import Theam from './theme';
+import './Header.css';
+
+  
 
 export default class Header extends Component {
+    
+
     render() {
         return (
             <div>
-                <ul>
-                    { this.props.token !== '' && <div>Welcome!</div> }
-                    { this.props.token !== '' && <Link to="/SearchPage"><div>Search Cities</div></Link> }
-                    <Link to="/SignInPage"><div>Log In</div></Link>
-                    <Link to="/SignUpPage"><div>Sign Up</div></Link>
-                    <button onClick={() =>this.props.handleUserChange('')}>Logout User</button>
+                <ul style={ { backgroundColor: Theam.palette.secondary.main }}>
+                    { this.props.token === '' && <li><Link to="/SignInPage"><div>Log In</div></Link></li> }
+                    {this.props.token === '' && <li><Link to="/SignUpPage"><div>Sign Up</div></Link></li>}
+                    { this.props.token !== '' && <li><div>Welcome!</div></li> }
+                    {this.props.token !== '' && <li><Link to="/SearchPage"><div>Search Cities</div></Link></li>}
+                    {this.props.token !== '' && <li><Link to="/StarredListpage"><div>Starred List</div></Link></li>}
+                    { <li><Link to="/abutUs"><div>About Us</div></Link> </li>}
+                    {/* <button onClick={() => this.props.handleUserChange('')}>Logout User</button> */}
+                    {this.props.token !== '' && <li><Link onClick={() => this.props.handleUserChange('')} to="/SignInPage"><div>Logout</div></Link></li>}
                 </ul>
             </div>
         )
---
+++ src/aboutUs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class aboutUs extends Component {
+    render() {
+        return (
+            <div>
+                about us
+            </div>
+        )
+    }
+}
