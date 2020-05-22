53c671f5dfbe8a457f933722bc232bb5d2de5fa9
langstonBS <langston.beckwithstanley@gmail.com>
2020-05-22 12:46:08 -0700

    we are done

--- src/Calendar.js
+++ src/Calendar.js
@@ -5,32 +5,33 @@ import 'react-calendar/dist/Calendar.css';
 import './Calendar.css'
 
 export default class Sample extends Component {
-  state = {
-    value: new Date(),
-  }
+    state = {
+        value: new Date(),
+    }
 
-  onChange = value => this.setState({ value })
+    onChange = value => this.setState({ value })
 
-  render() {
-      const { value } = this.state;
-      console.log(value);
+    render() {
+        const { value } = this.state;
+        console.log(value);
 
-    return (
-      <div className="Sample">
-        <header>
-          <h1>Date of Journal</h1>
-        </header>
-          <div className="Sample__container">
-          <main className="Sample__container__content">
-            <Calendar
-              onChange={this.onChange}
-              hover
-              tileContent	
-              value={value}
-            />
-          </main>
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div className="Sample">
+                <header>
+                    <h1>Date of Journal</h1>
+                </header>
+                <div className="Sample__container">
+                    <main className="Sample__container__content">
+                        <Calendar
+                            onChange={this.onChange}
+                            hover
+                            tileContent
+                            value={value}
+                        />
+                    </main>
+                </div>
+            </div>
+        );
+    }
 
+}
