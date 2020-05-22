c0d0af0298642672d31685740ecc3b3b61bcc556
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-22 11:48:55 -0700

    edits aboutUs.js

--- src/aboutUs.js
+++ src/aboutUs.js
@@ -6,17 +6,15 @@ import './aboutUs.css'
 export default class aboutUs extends Component {
     render() {
         return (
-            <>
+            <div>
                 <ul>
                     <Card name={'kigers'}
                         alt={'space the finial frontear'}
                         photo={kPhoto}
-                        detail={"Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod"} />
-                    
-
+                        detail={"Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod"} 
+                    />
                 </ul>
-               
-            </>
+            </div>
         )
     }
 }
